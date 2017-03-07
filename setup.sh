d893c7b118dfd535dc8a2d6ec3fdf140f1e40135
Rene Zubcevic <Rene Zubcevic>
2017-03-07 21:57:12 +0100

    made some pre test functions

--- setup.sh
+++ setup.sh
@@ -4,9 +4,13 @@ trap shutdown INT SIGTERM
 
 shutdown() {
 
-	echo
-	echo 'Thank you'
-	exit 0
+	if [ "$1" != 0 ] ; then 
+		printf -- "\nExit $1: $2\n"
+		
+	fi
+	printf -- "Thank you for using the vagrant setup tool\n"
+	printf -- "Copyright (c) Zubcevic.com 2017\n"
+	exit $1
 
 }
 
@@ -24,26 +28,35 @@ else
 fi
 }
 
-generatesshkey "github_rsa"
 
+checkdisksize() {
 disksize=$(df -k -l --output=avail / |tail -n1)
 if  [[ $disksize -lt 30000000 ]] ; then
-	echo "not more than 30GB free"
+	shutdown 1 "not more than 30GB free"
 fi
+}
 
+checkcpusize() {
 cpusize=$(cat /proc/cpuinfo | grep processor| wc -l)
 if  [[ $cpusize -lt 5 ]] ; then
-	echo "not enough cpu"
+	shutdown 1 "not enough cpu"
 fi
+}
 
+checkmemsize() {
 memsize=$(cat /proc/meminfo | grep MemFree| awk '{print $2}' )
 if  [[ $memsize -lt 4500000 ]] ; then
-	echo "not enough free memory"
+	shutdown 1 "not enough free memory"
 fi
+}
 
-echo $disksize
-echo $cpusize
-echo $memsize
+generatesshkey "github_rsa"
+
+checkdisksize
+checkcpusize
+checkmemsize
+
+curl https://www.eclipse.org/downloads/download.php?file=/oomph/epp/neon/R2a/eclipse-inst-linux64.tar.gz&r=1 -o downloads/eclipse.tar.gz
 
 if vagrant status | grep -q 'not created' ; then
 	vagrant plugin install vagrant-vbguest
