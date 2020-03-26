69ad8d82a3683132b0c33ca3ea65290174721dfb
René Zubcevic <rene@zubcevic.com>
2020-03-26 11:00:50 +0100

    assets for image

---
+++ assets/startcrs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+docker run -dti \
+--name apachecrsrp \
+--env PARANOIA=1 \
+--env ANOMALYIN=5 \
+--env ANOMALYOUT=5 \
+--env ALLOWED_METHODS="GET POST" \
+--env MAX_FILE_SIZE=5242880 \
+--env RESTRICTED_EXTENSIONS=".conf/" \
+--env PORT=8001 \
+--publish 8001:8001 \
+--env BACKEND=http://12.0.0.1:8080 \
+--restart unless-stopped \
+franbuehler/modsecurity-crs-rp
