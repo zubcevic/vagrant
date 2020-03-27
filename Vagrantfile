8470c262be253469f5a0282665eb700320cd2e41
René Zubcevic <rene@zubcevic.com>
2020-03-27 10:14:32 +0100

    some updates

--- Vagrantfile
+++ Vagrantfile
@@ -5,15 +5,17 @@ Vagrant.configure("2") do |config|
   config.vm.box_version = "1905.1"
 
   config.vm.network "forwarded_port", guest: 9080, host: 9080
-  #config.vm.synced_folder "/downloads", "/downloads"
+  config.vm.network "private_network", ip: "12.0.0.1"
+  #config.vm.synced_folder ".", "/downloads"
 
   # Provider-specific configuration so you can fine-tune various
   config.vm.provider "virtualbox" do |vb|
 	vb.name = "devbox_webgoat"
 	vb.memory = "4096"
 	vb.cpus = 2
+  vb.gui = true
    end
-  
+
    config.vm.provision "shell", path: "provision.sh", privileged: false
 
 end
--- provision.sh
+++ provision.sh
@@ -4,6 +4,7 @@ sudo yum update -y
 sudo hostname www.webgoat.local
 sudo timedatectl set-timezone CET
 
+sudo yum -y install zip unzip wget git
 sudo yum -y groups install "GNOME Desktop"
 sudo systemctl set-default graphical.target
 sudo systemctl isolate graphical.target
@@ -22,3 +23,38 @@ sudo usermod -aG docker vagrant
 sudo systemctl enable docker
 sudo systemctl start docker
 
+# INSTALLATION OF OWASP ZAP
+if [! -f zap.tar.gz]
+then
+wget https://github.com/zaproxy/zaproxy/releases/download/v2.8.1/ZAP_2.8.1_Linux.tar.gz -O zap.tar.gz
+tar -xf zap.tar.gz
+fi
+
+# INSTALLATION OF JAVA11
+if [! -f jre11.tar.gz]
+then
+wget https://github.com/AdoptOpenJDK/openjdk11-binaries/releases/download/jdk-11.0.5%2B10/OpenJDK11U-jre_x64_linux_hotspot_11.0.5_10.tar.gz -O jre11.tar.gz
+tar -xf jre11.tar.gz
+echo "export PATH=$PWD/jdk-11.0.5+10-jre/bin:$PATH" >> .bash_profile
+fi
+
+# INSTALLATION OF FIREFOX
+wget https://download.mozilla.org/?product=firefox-latest-ssl\&os=linux64 -O firefox.tar.bz2
+tar -xf firefox.tar.bz2
+
+# RUN OWASP WebGoat and WebWolf
+docker run -d -p 8080:8080 -p 9090:9090 -p 80:80 -e TZ=Europe/Amsterdam -name webgoat webgoat/goatandwolf
+
+# RUN OWASP CRS ModSecurity
+docker run -dti \
+--name apachecrsrp \
+--env PARANOIA=1 \
+--env ANOMALYIN=5 \
+--env ANOMALYOUT=5 \
+--env ALLOWED_METHODS="GET POST" \
+--env MAX_FILE_SIZE=5242880 \
+--env RESTRICTED_EXTENSIONS=".conf/" \
+--env PORT=8001 \
+--publish 8001:8001 \
+--env BACKEND=http://10.56.14.8:8080 \
+franbuehler/modsecurity-crs-rp
